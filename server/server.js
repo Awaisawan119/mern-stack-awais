b3256541fde1bc9c0ab7d753f9ef026fc55f6f82
Awaisawan119 <malikawaisimran586@gmail.com>
2025-08-23 23:30:28 +0500

    modify

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-require("dotenv").config()
-const connectDB = require("./config/db")
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-// connect to the mongodb database
-/* connectDB() */
+// connect to MongoDB
+connectDB();
 
-app.use('/api/items', require("./routes/items"))
-app.use('/api/payment', cors(), require("./routes/payment"))
+// routes
+app.use('/api/items', require("./routes/items"));
+app.use('/api/payment', require("./routes/payment"));
 
-app.listen(PORT, console.log("Server is running on port ", PORT))
+// start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
